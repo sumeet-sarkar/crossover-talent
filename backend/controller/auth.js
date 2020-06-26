cbcf26485ea774cf923a517520e521fa9caabd63
unknown <sudarshan97.kudli@gmail.com>
2020-06-26 20:58:46 +0530

    bug fix in home api 2

--- backend/controller/auth.js
+++ backend/controller/auth.js
@@ -76,7 +76,6 @@ exports.login = (req, res, next) => {
             }, 
             'secret',
             { expiresIn: '2h' });
-            req.session.loggedIn = true;
             res.status(200).json({ token: token, userId: loadedUser._id.toString() });
         })
         .catch(err => {
