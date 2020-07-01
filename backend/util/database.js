a6fc444afe2ce326c645eb6baa3ea518a623e305
unknown <sudarshan97.kudli@gmail.com>
2020-07-01 16:15:01 +0530

    home api updated to send job IDs of user applications.Started working on unit testing

--- backend/util/database.js
+++ backend/util/database.js
@@ -2,12 +2,14 @@ const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 
 let _db;
+let _client;
 //iLoveMongodbMyHobbyIsCoding1234
 const mongoConnect = (cb) => {
     MongoClient.connect('mongodb+srv://kudli:iLoveMongodbMyHobbyIsCoding1234@main-qrwun.mongodb.net/crossover-talent?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
         .then(client => {
             console.log("Connected to MongoDb Atlas");
             _db = client.db();
+            _client = client;
             cb();
         })
         .catch(err => {
@@ -23,5 +25,13 @@ const getDb = () => {
     throw "No database Found";
 }
 
+const getClient = () => {
+    if (_client) {
+        return _client;
+    }
+    throw "No client found";
+}
+
 exports.mongoConnect = mongoConnect;
 exports.getDb = getDb;
+exports.getClient = getClient;
